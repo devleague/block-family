2b9b3d65d5024c403af21330696d8d4c0a154b6d
sgnl <rayrfarias@gmail.com>
2016-12-16 13:42:43 -1000

    removes gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var connect = require('gulp-connect');
- 
-gulp.task('connect', function(){
-  connect.server({
-    root: 'public',
-    livereload: true
-  });
+
+gulp.task('browser-sync', function(){
+
 });
- 
+
 // keeps gulp from crashing for scss errors
 gulp.task('sass', function () {
   return gulp.src('./sass/*.scss')
-      .pipe(sass({ errLogToConsole: true }))
-      .pipe(gulp.dest('./public/css'));
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(gulp.dest('./public/css'));
 });
- 
-gulp.task('livereload', function (){
-  gulp.src('./public/**/*')
-  .pipe(connect.reload());
-});
- 
+
 gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
   gulp.watch('./public/**/*', ['livereload']);
 });
- 
-gulp.task('default', ['connect', 'watch', 'sass']);
+
+gulp.task('default', ['browser-sync', 'watch', 'sass']);
